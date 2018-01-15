675aa7d0a436b7b5d46ceabb0e7c1e992673d644
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2018-01-15 20:34:38 +0000

    store email tsting

--- src/app/indexed-db/indexed-db.component.ts
+++ src/app/indexed-db/indexed-db.component.ts
@@ -12,4 +12,30 @@ export class IndexedDBComponent implements OnInit {
   ngOnInit() {
   }
 
+  storeEmailLocalStorage() {
+
+    // Open connection to local database
+    this.request = indexedDB.open(this.sabreObjectStore, 1);
+
+    // Store database object request
+    this.request.onsuccess = (event) => {
+      this.db = event.target.result;
+
+      // this.addData();
+    }
+
+    // Handle Upgrade of database.
+    this.request.onupgradeneeded = (event) => {
+      let upgradedDb = event.target.result;
+
+      upgradedDb.onerror = (event) => {
+        alert('Error ' + event)
+      }
+
+      let objectStore = this.db.createObjectStore('usernames', { keyPath: 'email' });
+
+      objectStore.createIndex('index', 'email', { unique: true, });
+
+    } // end upgrade
+
 }
