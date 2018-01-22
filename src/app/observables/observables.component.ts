1859a5e93ddbf3d2acc03021f0d9318ed80ec14f
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2018-01-22 21:02:42 +0000

    'removal of old code'

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -9,45 +9,10 @@ import { Observable } from 'rxjs/Rx';
 })
 export class ObservablesComponent implements OnInit {
 
-  dataObject = ([
-    { name: 'micahel', age: '57', pets: 'bubbles', wife: 'lisa-marie'},
-    { name: 'kanye',  age: '37', pets: 'tiger', wife: 'kim' },
-  ]
-);
-
-  storedNames = [];
-
   constructor() { }
 
   ngOnInit() {
-    let obsArray = Observable.from(this.dataObject);
 
-    obsArray.subscribe(results => {
-      console.log(results);
-    })
-
-    obsArray
-    .filter(objName => objName.name == 'kanye')
-    .mapTo('Kanye')
-    .subscribe(results => console.log('Result of observable :',results));
-
-    console.log('Result of for loop :' ,this.forLoopTest(this.dataObject));
   }
-
-  forLoopTest(dataObject) {
-    let i;
-
-    for (i=0; this.dataObject.length; i++){
-      let names = this.dataObject[i];
-
-      this.storedNames.push(names);
-
-      return this.storedNames;
-    }
-  }
-
-
-
-  
 
 }
