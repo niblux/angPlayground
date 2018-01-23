ca0143f4d9e8d087edc37e17a020a24f4770d60c
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2018-01-23 23:21:12 +0000

    Updated practice array functions

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -12,6 +12,8 @@ export class ObservablesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    // MAP Array Function (Iterator/Mutator)
     Array.prototype.map = function (projectionFunction) {
       var results = [];
       this.forEach(function (itemInArray) {
@@ -21,6 +23,30 @@ export class ObservablesComponent implements OnInit {
 
       return results;
     };
+
+    // Reduce Array Function (Iterator)
+    Array.prototype.reduce = function (projectionFunction) {
+      var results = [];
+      this.forEach(function (itemInArray) {
+        results.push();
+
+      });
+
+      return results;
+    };
+
+    // ?? Scan Array Function (Iterator)
+    Array.prototype.scan = function (projectionFunction) {
+      var results = [];
+      this.forEach(function (itemInArray) {
+        results.push();
+
+      });
+
+      return results;
+    };
+
+
 
 // JSON.stringify([1,2,3].map(function(x) { return x + 1; })) === '[2,3,4]'
   }
